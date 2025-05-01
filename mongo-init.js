3d473898224d9b8fa6406b643dacbf6fdad1011e
ke52usel@studserv.uni-leipzig.de <56174409+fansel@users.noreply.github.com>
2025-05-01 16:24:26 +0200

    change init

--- mongo-init.js
+++ mongo-init.js
@@ -1,25 +1,31 @@
 db = db.getSiblingDB('admin')
 
-db.createUser({
-  user: 'huegelfest_admin',
-  pwd: 'huegelfest_admin_password',
-  roles: [
-    {
-      role: 'root',
-      db: 'admin'
-    }
-  ]
-})
+// Prüfe ob der Admin-Benutzer bereits existiert
+if (!db.getUser("huegelfest_admin")) {
+  db.createUser({
+    user: 'huegelfest_admin',
+    pwd: 'huegelfest_admin_password',
+    roles: [
+      {
+        role: 'root',
+        db: 'admin'
+      }
+    ]
+  })
+}
 
 db = db.getSiblingDB('huegelfest')
 
-db.createUser({
-  user: 'huegelfest_user',
-  pwd: 'huegelfest_password',
-  roles: [
-    {
-      role: 'readWrite',
-      db: 'huegelfest'
-    }
-  ]
-}) 
+// Prüfe ob der Anwendungsbenutzer bereits existiert
+if (!db.getUser("huegelfest_user")) {
+  db.createUser({
+    user: 'huegelfest_user',
+    pwd: 'huegelfest_password',
+    roles: [
+      {
+        role: 'readWrite',
+        db: 'huegelfest'
+      }
+    ]
+  })
+} 
