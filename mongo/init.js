a35abbea5d9389203a923d9294d05636b503ed67
ke52usel@studserv.uni-leipzig.de <56174409+fansel@users.noreply.github.com>
2025-05-04 02:30:43 +0200

    update syntax

--- mongo/init.js
+++ mongo/init.js
@@ -1,12 +1,25 @@
+function getMongoConfig() {
+  const username = process.env.MONGO_USERNAME;
+  const password = process.env.MONGO_PASSWORD;
+
+  if (!username || !password) {
+    console.error('MongoDB Zugangsdaten sind nicht konfiguriert');
+    throw new Error('MongoDB Zugangsdaten sind nicht konfiguriert');
+  }
+
+  return { username, password };
+}
+
+// Wechsle zur Anwendungsdatenbank
 db = db.getSiblingDB('huegelfest');
 
+const { username, password } = getMongoConfig();
+
+// Erstelle den Anwendungsbenutzer
 db.createUser({
-  user: process.env.MONGO_USERNAME,
-  pwd: process.env.MONGO_PASSWORD,
+  user: username,
+  pwd: password,
   roles: [
-    {
-      role: "readWrite",
-      db: "huegelfest"
-    }
+    { role: 'readWrite', db: 'huegelfest' }
   ]
 }); 
