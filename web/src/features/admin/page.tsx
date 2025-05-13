c97257b56199d7081329999fc64d88fbfa8e89e6
ke52usel@studserv.uni-leipzig.de <56174409+fansel@users.noreply.github.com>
2025-05-13 02:25:34 +0200

    final jusic

--- web/src/features/admin/page.tsx
+++ web/src/features/admin/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { useState } from 'react';
 import AppLayout from '@/shared/components/AppLayout';
 import AdminDashboardWrapper from '../admin/dashboard/AdminDashboardWrapper';
